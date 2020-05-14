3358ae8017b049d1f86d0ae50243f5a02f9ad0d2
jisshub <jissmon476@gmail.com>
2020-05-14 16:29:32 +0530

    added redirect

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('register/', views.register_view, name='register')
+    path('register/', views.register_view, name='register'),
 ] 
--- user/views.py
+++ user/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 # import message module
 from django.contrib import messages
+# import shortcuts here.
+from django.shortcuts import redirect
+
 # Create your views here.
 
 def register_view(request):
@@ -16,8 +19,15 @@ def register_view(request):
             username = form.cleaned_data.get('username')
             # flash message if success,
             messages.success(request, f'Account created for {username}!') 
+            return redirect('home')
     else:
         # create form instance if request not post
         form = UserCreationForm()
     # render the form
     return render(request, 'user/register.html', {"form": form})
+
+
+def login_view(request):
+    pass
+    return render(request, 'user/login.html')
+
